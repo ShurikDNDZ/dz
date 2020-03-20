9eed3d5a38a7ecde4a7f672d495948d3283f28c3
ShurikDNDZ <ShurikDNDZ@ukr.net>
2020-03-20 16:26:05 +0200

    paint

---
+++ data/config.py
@@ -0,0 +1,7 @@
+WIDTH = 700
+HEIGHT = 500
+
+BRUSH_SIZE = 15
+DEFAULT_COLOR = 'black'
+BG_COLOR = 'white'
+ERASE_COLOR = 'white'
---
+++ paint.py
@@ -0,0 +1,27 @@
+# import
+
+
+def active_paint(event):
+    pass
+
+
+def draw(event):
+    pass
+
+
+def decrease_brush_size():
+    pass
+
+
+def increase_brush_size():
+    pass
+
+	
+def color_change(new_color):
+    pass
+
+
+def get_color():
+    pass
+
+
